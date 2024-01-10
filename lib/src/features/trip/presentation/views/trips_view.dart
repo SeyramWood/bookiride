4b80516ebc0a02a4eec15f5aac048d6b12dda943
Stephen Essoun <boit7845@gmail.com>
2024-01-10 14:13:23 +0000

    logout implemented

--- lib/src/features/trip/presentation/views/trips_view.dart
+++ lib/src/features/trip/presentation/views/trips_view.dart
@@ -1,7 +1,12 @@
+import 'package:bookihub/src/features/authentication/presentation/view/login_view.dart';
 import 'package:bookihub/src/features/trip/presentation/views/completed_trip_view.dart';
 import 'package:bookihub/src/features/trip/presentation/views/scheduled_trip_view.dart';
 import 'package:bookihub/src/features/trip/presentation/views/todays_trip_view.dart';
 import 'package:bookihub/src/shared/constant/dimensions.dart';
+import 'package:bookihub/src/shared/utils/alert_dialog.dart';
+import 'package:bookihub/src/shared/utils/interceptor.dart';
+import 'package:bookihub/src/shared/utils/show.snacbar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../shared/utils/exports.dart';
 
@@ -25,10 +30,40 @@ class _TripsViewState extends State<TripsView> {
           padding: const EdgeInsets.symmetric(horizontal: hPadding),
           child: Column(
             children: [
-              vSpace,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        showCustomDialog(context,
+                            const Text('By continuing will logout you out'),
+                            () async {
+                          storage.deleteAll();
+                          var pref = await SharedPreferences.getInstance();
+                          pref.clear();
+
+                          if (mounted) {
+                            showCustomSnackBar(
+                                context, 'successfully logged out', green);
+                            Navigator.of(context).pushAndRemoveUntil(
+                                MaterialPageRoute(
+                                  builder: (context) => const LoginView(),
+                                ),
+                                (route) => false);
+                          }
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.power_settings_new_rounded,
+                        color: orange,
+                      ))
+                ],
+              ),
+              // vSpace,
               SizedBox(
                 height: MediaQuery.sizeOf(context).height * 0.045,
                 child: ListView.separated(
+                    physics: const NeverScrollableScrollPhysics(),
                     separatorBuilder: (context, index) => SizedBox(
                           width: MediaQuery.sizeOf(context).width * .045,
                         ),
